87e5b5097bed5b9d303a4d06b2b72defa4a4bede
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-12-01 13:08:03 -0500

    update

--- AoC2024/00_day1.py
+++ AoC2024/00_day1.py
@@ -1,4 +1,4 @@
-with open("input", "r") as file:
+with open("test", "r") as file:
 
     left = []
     right = []
@@ -10,9 +10,15 @@ with open("input", "r") as file:
         left.sort()
         right.sort()
 
-    sum_diff = 0
-    for i, k in zip(left, right):
-        diff = abs(i - k)
-        sum_diff += diff
+    # sum_diff = 0  # for part 1
+    # for i, k in zip(left, right):  # for part 1
+        # diff = abs(i - k)  # for part 1
+        # sum_diff += diff  # for part 1
 
-    print(sum_diff)
+    for i in left:
+        times_appeared = 0
+        for k in right:
+            if i == k:
+                times_appeared += 1
+
+    print(times_appeared)
